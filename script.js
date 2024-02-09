550c21ea0ea6138e063764a11bbaf2fbaf5c01e8
nyepudolo <142420110+nyepudolo@users.noreply.github.com>
2024-02-09 17:18:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Blogger', 'Editor'],
+    strings: ['Database Management', 'Analytics', 'Financial Fraud Detection'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
